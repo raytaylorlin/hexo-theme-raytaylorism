9a1aa57a3cbe21a4e5bba8ef464a9f10ffa3a132
水月轻枫 <raytaylorlin@gmail.com>
2013-08-14 22:22:56 +0800

    1.加入字体fontawesome、fontdiao，CSS库furatto 2.完成header基本排版

--- source/js/animation.js
+++ source/js/animation.js
@@ -1,18 +1,25 @@
 (function($) {
     var widget = $('.widget-wrapper');
+    var isOpened = false;
     var widgetWidth = widget.width();
     widget.css('margin-left', '-' + widgetWidth + 'px');
 
     $('.category-tag').mouseenter(
         function() {
-            widget.animate({
-                marginLeft: "+=" + widgetWidth
-            }, "normal");
+            if (!isOpened) {
+                widget.animate({
+                    marginLeft: "+=" + widgetWidth
+                }, "normal");
+                isOpened = true;
+            }
         });
     widget.mouseleave(
         function() {
-            widget.animate({
-                marginLeft: "-=" + widgetWidth
-            }, "normal");
+            if (isOpened) {
+                widget.animate({
+                    marginLeft: "-=" + widgetWidth
+                }, "normal");
+                isOpened = false;
+            }
         });
 })(jQuery);
